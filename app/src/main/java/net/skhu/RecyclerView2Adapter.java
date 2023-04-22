f82febc05da42b0148244d0073fa79e0df1fc4e7
Kim gyeongE <binggre05@naver.com>
2023-04-23 02:55:22 +0900

    first commit

---
+++ app/src/main/java/net/skhu/RecyclerView2Adapter.java
@@ -0,0 +1,64 @@
+package net.skhu;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class RecyclerView2Adapter extends RecyclerView.Adapter<RecyclerView2Adapter.ViewHolder> {
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView textView1, textView2;
+
+        public ViewHolder(View view) {
+            super(view);
+            textView1 = view.findViewById(R.id.textView1);
+            textView2 = view.findViewById(R.id.textView2);
+            view.setOnClickListener(this);
+        }
+
+        public void setData(int index) {
+            Memo2 memo = arrayList.get(index);
+            textView1.setText(memo.getTitle());
+            textView2.setText(memo.getDateFormatted());
+        }
+
+        @Override
+        public void onClick(View view) {
+            int index = super.getAdapterPosition();
+            Memo2 memo = arrayList.get(index);
+            String s = String.format("index: %d,  title: %s", index, memo.getTitle());
+            Toast.makeText(view.getContext(), s, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    LayoutInflater layoutInflater;
+    ArrayList<Memo2> arrayList;
+
+    public RecyclerView2Adapter(Context context, ArrayList<Memo2> arrayList) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = layoutInflater.inflate(R.layout.memo2, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder viewHolder, final int index) {
+        viewHolder.setData(index);
+    }
+}
+
